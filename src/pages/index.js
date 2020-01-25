6d5a2d1913c313ff024db1a9ccefaf0d4948b91e
Wryhder <jemimahsummers@yahoo.com>
2020-01-25 21:58:40 +0100

    Customize Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import "bulma/css/bulma.min.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="has-text-centered" style={{ marginTop: "20%" }}>
+      <h1 className="is-size-2">Scroll Image Gallery with Gatsby and Netlify functions</h1>
+      <p className="is-size-5" style={{ marginTop: "2%" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto velit odit nihil repudiandae et quasi ratione molestias repellendus ullam eaque, libero commodi voluptatibus nam vel architecto, dolor quis provident aperiam sequi molestiae neque! Eius cum facere, pariatur fuga repudiandae architecto officiis ratione vero accusantium totam adipisci quo aperiam repellendus dolor.
+      </p>
+      <button className="button is-dark is-large" style={{ marginTop: "10%" }}>
+        <Link to="/gallery/" className="has-text-white">
+          Gallery
+        </Link>
+      </button>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
