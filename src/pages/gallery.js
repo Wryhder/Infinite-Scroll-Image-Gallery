7269d4f5755a9be8016f4a967618bc2f81a47689
Wryhder <jemimahsummers@yahoo.com>
2020-01-25 21:59:17 +0100

    Create Gallery page with initial content

---
+++ src/pages/gallery.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Gallery = () => {
+  return (
+    <Layout>
+      <SEO title="Gallery" />
+      <h1 className="is-size-3">Infinite Scroll Gallery</h1>
+      <p style={{ marginBottom: "5%" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eveniet officiis beatae assumenda, tenetur nulla eius explicabo ea placeat magnam?</p>
+    </Layout>
+    )
+}
+
+export default Gallery
