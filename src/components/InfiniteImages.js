4209bbfa2b4693892d44f1c31d45bceacb21a0ea
Wryhder <jemimahsummers@yahoo.com>
2020-01-28 15:03:45 +0100

    Prettier formatting

--- src/components/InfiniteImages.js
+++ src/components/InfiniteImages.js
@@ -7,22 +7,22 @@ import "./gallery.css"
 
 const ImageGallery = ({ images, loading, fetchImages }) => {
   return (
-    <InfiniteScroll 
-    dataLength={images.length} 
-    next={() => fetchImages()} 
-    hasMore={true} 
+    <InfiniteScroll
+      dataLength={images.length}
+      next={() => fetchImages()}
+      hasMore={true}
       loader={<p style={{ textAlign: "center", marginTop: "1%" }}>.....</p>}
     >
       <div className="image-grid">
         {!loading
           ? images.map(image => (
-            <div className="image-item" key={image.id}>
+              <div className="image-item" key={image.id}>
                 <img src={image.urls.regular} alt={image.alt_description} />
-            </div>
-          ))
-        : ""}
+              </div>
+            ))
+          : ""}
       </div>
-      </InfiniteScroll>
+    </InfiniteScroll>
   )
 }
 
